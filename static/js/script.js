2ecb9c83bc348760fad9f93a12d3a75641686f8f
bon4to <lucasbon4to@gmail.com>
2024-06-13 07:33:55 -0300

    dark-theme

--- static/js/script.js
+++ static/js/script.js
@@ -283,6 +283,25 @@ function togglePopUp() {
     }
 }
 
+const toggleTheme = document.getElementById('toggle-theme');
+toggleTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+
 window.addEventListener("load", function() {
     hideLoading();
 });
