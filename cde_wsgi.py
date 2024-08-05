efb521d66c32ddb10f56a756b7c17c16628f5791
bon4to <lucasbon4to@gmail.com>
2024-08-05 17:15:33 -0300

    minor changes

--- cde_wsgi.py
+++ cde_wsgi.py
@@ -5,15 +5,15 @@ logger = logging.getLogger('waitress')
 logger.setLevel(logging.ERROR)
 
 class ANSI:
-    RESET = "\u001b[0m"
-    BOLD = "\u001b[1m"
-    RED = "\u001b[31m"
-    GREEN = "\u001b[32m"
-    YELLOW = "\u001b[33m"
-    BLUE = "\u001b[34m"
-    MAGENTA = "\u001b[35m"
-    CYAN = "\u001b[36m"
-    WHITE = "\u001b[37m"
+    RESET = ""
+    BOLD = ""
+    RED = ""
+    GREEN = ""
+    YELLOW = ""
+    BLUE = ""
+    MAGENTA = ""
+    CYAN = ""
+    WHITE = ""
 
 
 class TAGS:
