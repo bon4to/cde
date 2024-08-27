2a35fd629d3a8a5f3191b591038e28f3d5aeab47
bon4to <lucasbon4to@gmail.com>
2024-08-27 08:16:11 -0300

    update schemas generator

--- cde.py
+++ cde.py
@@ -182,48 +182,49 @@ def create_tables():                                                            
 # TABELA DE PROGRAMAÇÃO DO ENVASE
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS prog_envase (
-                id_envase        INTEGER PRIMARY KEY AUTOINCREMENT,
-                cod_linha        INTEGER(3),
-                cod_cliente      INTEGER(10),
-                cod_item         VARCHAR(6),
-                qtde_solic       INTEGER(20),
-                data_entr_antec  DATETIME,
-                data_envase      DATETIME,
-                observacao       VARCHAR(100),
-                flag_concluido   BOOLEAN DEFAULT FALSE
+                id_envase       INTEGER PRIMARY KEY AUTOINCREMENT,
+                cod_linha       INTEGER(3),
+                cod_cliente     INTEGER(10),
+                cod_item        VARCHAR(6),
+                qtde_solic      INTEGER(20),
+                data_entr_antec DATETIME,
+                data_envase     DATETIME,
+                observacao      VARCHAR(100),
+                flag_concluido  BOOLEAN DEFAULT FALSE
             );
         ''')
 
 # TABELA DE PROGRAMAÇÃO DA PROCESSAMENTO
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS prog_producao (
-                id_producao      INTEGER PRIMARY KEY AUTOINCREMENT,
-                cod_linha        INTEGER(3),
-                liq_tipo         VARCHAR(10),
-                liq_linha        VARCHAR(30),
-                liq_cor          VARCHAR(30),
-                embalagem        VARCHAR(10),
-                lts_solic        INTEGER(20),
-                data_entr_antec  DATETIME,
-                data_producao    DATETIME,
-                observacao       VARCHAR(100),
-                flag_concluido   BOOLEAN DEFAULT FALSE
+                id_producao     INTEGER PRIMARY KEY AUTOINCREMENT,
+                cod_linha       INTEGER(3),
+                liq_tipo        VARCHAR(10),
+                liq_linha       VARCHAR(30),
+                liq_cor         VARCHAR(30),
+                embalagem       VARCHAR(10),
+                lts_solic       INTEGER(20),
+                data_entr_antec DATETIME,
+                data_producao   DATETIME,
+                observacao      VARCHAR(100),
+                flag_concluido  BOOLEAN DEFAULT FALSE
             );
         ''')
 
 # TABELA HISTÓRICO
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS historico (
-                id_mov           INTEGER PRIMARY KEY AUTOINCREMENT,
-                rua_numero       INTEGER(6),
-                rua_letra        VARCHAR(10),
-                cod_item         VARCHAR(6),
-                lote_item        VARCHAR(8),
-                quantidade       INTEGER,
-                operacao         VARCHAR(15),
-                user_name        VARCHAR(30),   -- id_user INTEGER,
-                id_carga         INTEGER(6),
-                time_mov         DATETIME
+                id_mov     INTEGER PRIMARY KEY AUTOINCREMENT,
+                rua_numero INTEGER(6),
+                rua_letra  VARCHAR(10),
+                cod_item   VARCHAR(100),
+                lote_item  VARCHAR(8),
+                quantidade INTEGER,
+                operacao   VARCHAR(15),
+                user_name  VARCHAR(30),
+                time_mov   DATETIME,
+                id_carga   INTEGER(6), 
+                id_user    INTEGER
             );
         ''')
         
@@ -234,66 +235,76 @@ def create_tables():                                                            
                 razao_cliente    VARCHAR(100),
                 fantasia_cliente VARCHAR(100),
                 cidade_cliente   VARCHAR(100),
-                estado_cliente   VARCHAR(2)
+                estado_cliente   VARCHAR(30)
             );
         ''')
         
 # TABELA DE CARGAS PENDENTES
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS carga_pendente (
-                id_log           INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_carga         INTEGER(6),
-                cod_item         VARCHAR(6),
-                qtde_atual       INTEGER(20),
-                qtde_solic       INTEGER(20),
-                flag_pendente    BOOLEAN DEFAULT TRUE
+            CREATE TABLE IF NOT EXISTS carga_incomp (
+                id_log        INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_carga      INTEGER(6),
+                cod_item      VARCHAR(6),
+                qtde_atual    INTEGER(20),
+                qtde_solic    INTEGER(20),
+                flag_pendente BOOLEAN DEFAULT TRUE
             );
         ''')
 
 # TABELA DE USUÁRIOS
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (
-                id_user          INTEGER PRIMARY KEY AUTOINCREMENT,
-                login_user       VARCHAR(30) UNIQUE,
-                password_user    TEXT,
-                nome_user        VARCHAR(100),
-                sobrenome_user   VARCHAR(100),
-                privilege_user   INTEGER(2),
-                data_cadastro    DATETIME,
-                ult_acesso       DATETIME
+                id_user        INTEGER PRIMARY KEY AUTOINCREMENT,
+                login_user     VARCHAR(30) UNIQUE,
+                password_user  TEXT,
+                nome_user      VARCHAR(100),
+                sobrenome_user VARCHAR(100),
+                privilege_user INTEGER(2),
+                data_cadastro  DATETIME,
+                ult_acesso     DATETIME
             );
         ''')
 
 # TABELA DE PERMISSÕES DE USUÁRIO
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS user_permissions (
-                id_user          INTEGER,
-                id_perm          VARCHAR(6)
+                id_user INTEGER, 
+                id_perm VARCHAR(6)
             );
         ''')
 
 # TABELA DE ITENS
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS itens (
-                cod_item         INTEGER(6) PRIMARY KEY,
-                desc_item        VARCHAR(100),
-                dun14            INTEGER(14)
+                cod_item   VARCHAR(6) PRIMARY KEY,
+                desc_item  VARCHAR(100),
+                dun14      INTEGER(14),
+                flag_ativo BOOLEAN DEFAULT TRUE
             );
         ''')
 
 # TABELA AUXILIAR DE PRIVILÉGIOS
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS aux_privilege (
-                id_priv          INTEGER(2) PRIMARY KEY,
-                desc_priv        VARCHAR(30)
+                id_priv   INTEGER(2) PRIMARY KEY,
+                desc_priv VARCHAR(30) UNIQUE
             );
         ''')
 
 # TABELA AUXILIAR DE PERMISSÕES
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS aux_permissions (
-                id_perm          VARCHAR(6) PRIMARY KEY,
-                desc_perm        VARCHAR(30)
+                id_perm   VARCHAR(6) PRIMARY KEY,
+                desc_perm VARCHAR(100)
+            );
+        ''')
+        
+# TABELA AUXILIAR DE LINHAS
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS aux_linha (
+                cod_linha  INTEGER(3),
+                tipo_embal VARCHAR(10),
+                lit_embal  VARCHAR(10)
             );
         ''')
 
