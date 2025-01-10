99192f6d75f54b41487c47d46aea91cf169de13f
bonato <lucasbon4to@gmail.com>
2025-01-10 17:40:46 -0300

    refactor etiqueta generator

--- cde.py
+++ cde.py
@@ -2063,6 +2063,80 @@ class Schedule:
             return producao_list
 
 
+class EtiquetaGenerator:
+    IMAGE_SIZE = (400, 400)
+    FONT_PATH = 'arialbd.ttf'
+
+    @staticmethod
+    def generate(qr_text, desc_item, cod_item, cod_lote) -> str:
+        """
+        Gera uma etiqueta com QR Code e descrição.
+        Retorna a imagem em base64.
+        """
+        img = EtiquetaGenerator._create_base_image()
+        qr_image = misc.qr_code(qr_text)
+
+        EtiquetaGenerator._paste_qr_code(img, qr_image)
+        EtiquetaGenerator._draw_text(img, cod_item, desc_item, cod_lote)
+
+        return EtiquetaGenerator._image_to_base64(img)
+
+    @staticmethod
+    def _create_base_image():
+        """Cria a imagem base limpa."""
+        return Image.new('RGB', EtiquetaGenerator.IMAGE_SIZE, color='white')
+
+    @staticmethod
+    def _paste_qr_code(img, qr_image):
+        """Adiciona o QR Code na imagem centralizada."""
+        width, height = img.size
+        qr_width, qr_height = qr_image.size
+
+        # Redimensiona o QR Code se necessário
+        if qr_width > width or qr_height > height:
+            qr_image = qr_image.resize((width // 2, height // 2))
+
+        qr_x = (width - qr_width) // 2
+        qr_y = (height - qr_height) // 2
+        img.paste(qr_image, (qr_x, qr_y))
+
+    @staticmethod
+    def _draw_text(img, cod_item, desc_item, cod_lote):
+        """Desenha os textos na imagem."""
+        draw = ImageDraw.Draw(img)
+        width, height = img.size
+
+        # Configura fonte
+        font_large = ImageFont.truetype(EtiquetaGenerator.FONT_PATH, 30)
+        font_small = ImageFont.truetype(EtiquetaGenerator.FONT_PATH, 22)
+
+        # Desenha o código do lote
+        cod_lote_text = f'LOTE: {cod_lote}'
+        lote_bbox = draw.textbbox((0, 0), cod_lote_text, font=font_large)
+        lote_x = (width - (lote_bbox[2] - lote_bbox[0])) // 2
+        lote_y = height // 1.5
+        draw.text((lote_x, lote_y), cod_lote_text, fill='black', font=font_large)
+
+        # Desenha a descrição do item em múltiplas linhas
+        desc_text = f'{cod_item} - {desc_item}'
+        lines = textwrap.wrap(desc_text, width=30)
+        y_text = height - height // 4.6
+
+        for line in lines:
+            text_width, text_height = draw.textbbox((0, 0), line, font=font_small)[2:]
+            line_x = (width - text_width) // 2
+            draw.text((line_x, y_text), line, fill='black', font=font_small)
+            y_text += text_height
+
+    @staticmethod
+    def _image_to_base64(img):
+        """Converte a imagem para base64."""
+        img_io = io.BytesIO()
+        img.save(img_io, format='PNG')
+        img_io.seek(0)
+        return base64.b64encode(img_io.getvalue()).decode()
+
+
 class misc:
     @staticmethod
     # BUSCA FRASE PARA /INDEX
@@ -2086,42 +2160,6 @@ class misc:
         
         return timestamp.strftime('%Y/%m/%d %H:%M:%S')
     
-    
-    @staticmethod
-    # GERA ETIQUETA COM QRCODE
-    def generate_etiqueta(qr_text, desc_item, cod_item, cod_lote) -> str:
-        width, height = 400, 400
-        img  = Image.new('RGB', (width, height), color='white')
-        cod_lote = f'LOTE: {cod_lote}'
-        desc_item = f'{cod_item} - {desc_item}'
-
-        qr_image = misc.qr_code(qr_text)
-        qr_width, qr_height = qr_image.size
-        img.paste(qr_image, ((width - qr_width) // 2, (height - qr_height) // 2))
-
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype('arialbd.ttf', 30)
-
-        lote_bbox  = draw.textbbox((0, 0), cod_lote, font=font)
-        lote_width = lote_bbox[2] - lote_bbox[0]
-        draw.text(((width - lote_width) // 2, height // 1.5), cod_lote, fill='black', font=font)
-
-        text   = desc_item
-        font   = ImageFont.truetype('arialbd.ttf', 22)
-        lines  = textwrap.wrap(text, width=30)
-        y_text = height - height // 4.6
-        for line in lines:
-            text_width, text_height = draw.textbbox((0, 0), line, font=font)[2:]
-            draw.text(((width - text_width) // 2, y_text), line, font=font, fill='black')
-            y_text += text_height
-
-        img_io = io.BytesIO()
-        img.save(img_io, format='PNG')
-        img_io.seek(0)
-
-        img_base64 = base64.b64encode(img_io.getvalue()).decode()
-        return img_base64
-
 
     @staticmethod
     # RETORNA TIMESTAMP
@@ -4452,7 +4490,7 @@ def etiqueta() -> str:
         cod_item  = str(request.form['cod_item'])
         cod_lote  = str(request.form['lote_item'])
 
-        return misc.generate_etiqueta(qr_text, desc_item, cod_item, cod_lote)
+        return EtiquetaGenerator.generate(qr_text, desc_item, cod_item, cod_lote)
     return render_template('pages/etiqueta.html', produtos=produtos)
 
 
