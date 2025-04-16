516e9ef4ae9b18210a62fa49035767176243ef48
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-04-16 17:09:14 -0300

    fix sticker generator

--- app/models/stickerUtils.py
+++ app/models/stickerUtils.py
@@ -1,8 +1,9 @@
 import textwrap, io, base64, qrcode
 from PIL import Image, ImageDraw, ImageFont
 
+from app.models import logTexts
+
 IMAGE_SIZE = (400, 400)
-FONT_PATH = 'arialbd.ttf'
 
 @staticmethod
 # cria qrcode
@@ -16,7 +17,7 @@ def qr_code(qr_text):
     qr.add_data(qr_text)
     qr.make(fit=True)
     qr_image = qr.make_image(fill_color='black', back_color='white')
-
+    qr_image = qr_image.convert('RGB')
     return qr_image
 
 @staticmethod
@@ -33,10 +34,12 @@ def generate(qr_text, desc_item, cod_item, cod_lote) -> str:
 
     return _image_to_base64(img)
 
+
 @staticmethod
 def _create_base_image():
     """cria a imagem base limpa."""
     return Image.new('RGB', IMAGE_SIZE, color='white')
+
 
 @staticmethod
 def _paste_qr_code(img, qr_image):
@@ -50,7 +53,10 @@ def _paste_qr_code(img, qr_image):
 
     qr_x = (width - qr_width) // 2
     qr_y = (height - qr_height) // 2
-    img.paste(qr_image, (qr_x, qr_y))
+    # Specify the box with (left, top, right, bottom) coordinates
+    box = (qr_x, qr_y, qr_x + qr_width, qr_y + qr_height)
+    img.paste(qr_image, box)
+
 
 @staticmethod
 def _draw_text(img, cod_item, desc_item, cod_lote):
@@ -59,8 +65,20 @@ def _draw_text(img, cod_item, desc_item, cod_lote):
     width, height = img.size
 
     # configura fonte
-    font_large = ImageFont.truetype(FONT_PATH, 30)
-    font_small = ImageFont.truetype(FONT_PATH, 22)
+    try:
+        # fonte Arial para Windows
+        font_large = ImageFont.truetype("arialbd.ttf", 30)
+        font_small = ImageFont.truetype("arialbd.ttf", 22)
+    except:
+        # fonte Arial para Mac OS
+        try:
+            font_large = ImageFont.truetype("Arial Bold.ttf", 30)
+            font_small = ImageFont.truetype("Arial Bold.ttf", 22)
+        except:
+            # usa fonte padrão da biblioteca
+            logTexts.log("Erro ao carregar fonte Arial, é recomendável baixar e utilizar a fonte 'arialbd.ttf'")
+            font_large = ImageFont.load_default()
+            font_small = ImageFont.load_default()
 
     # desenha o código do lote
     cod_lote_text = f'LOTE: {cod_lote}'
@@ -79,6 +97,7 @@ def _draw_text(img, cod_item, desc_item, cod_lote):
         line_x = (width - text_width) // 2
         draw.text((line_x, y_text), line, fill='black', font=font_small)
         y_text += text_height
+
 
 @staticmethod
 def _image_to_base64(img):
